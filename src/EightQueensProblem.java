97e4d05212c164d88f912bf5378aba1f1eb87c8f
Scott-Salerno74 <sws001@lvc.edu>
2019-09-30 14:10:44 -0400

    Queens Problem Updated

--- src/EightQueensProblem.java
+++ src/EightQueensProblem.java
@@ -1,29 +1,30 @@
 import java.util.List;
 
 public class EightQueensProblem implements OptimizationProblem<QueenState, QueenAction> {
-
+   private QueenState queenInit = new QueenState(8);
     @Override
     public QueenState getInitialState() {
-        return null;
+        return queenInit;
     }
 
     @Override
     public List<QueenAction> getActions(QueenState state) {
-        return null;
+        return state.actionList;
     }
 
     @Override
     public QueenState getResult(QueenState state, QueenAction action) {
-        return null;
+        return state.act(action);
     }
 
     @Override
     public boolean testGoal(QueenState state) {
-        return false;
+        return state.isGoal();
     }
 
     @Override
     public double getObjective(QueenState State) {
-        return 0;
+        int temp = Integer.MAX_VALUE - State.heuristic();
+        return (double) temp;
     }
 }
